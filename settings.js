39748d526880cdf6a1a13ad257eb69326c8a716f
romi049 <159658987+romi049@users.noreply.github.com>
2024-05-26 22:40:12 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,7 +22,7 @@ global.mess = {
     endLimit: '`Your daily limit has expired, the limit will be reset every 12 hours`',
 }
 
-global.owner = [process.env.OWNER_NUMBER] || ['923131613251'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
+global.owner = [process.env.OWNER_NUMBER] || ['923321206850'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
 
 module.exports = {    
 SESSION_ID: process.env.SESSION_ID || 'youre session id',
@@ -39,7 +39,7 @@ ALWAYS_ONLINE:  process.env.ALWAYS_ONLINE  || false  ,
 WORK_TYPE: process.env.WORK_TYPE || 'private'  ,
 ANTI_LINK: process.env.ANTI_LINK || false  ,
 ANTI_BOT: process.env.ANTI_BOT || false  ,
-OWNER_NUMBER: process.env.OWNER_NUMBER || '923256245454'  ,    
+OWNER_NUMBER: process.env.OWNER_NUMBER || '923321206850'  ,    
 ALIVE: process.env.ALIVE || `default`,
 OWNER_NAME: process.env.OWNER_NAME || 'ROMI' ,     
 AUTO_STATUS_READ:  process.env.AUTO_STATUS_READ  || false  ,    
